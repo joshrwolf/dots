ddef80d9ac9cb5a8627f5520401aebb98c73b265
Josh Wolf <josh@wolfs.io>
2023-01-19 16:44:04 -0500

    switch (back) to wezterm and SF Mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,17 +9,92 @@ end)
 -- https://github.com/hurricanehrndz/cfg/blob/8993ea84ee04e2cf379d40a19851642078047e1e/dot_config/wezterm/wezterm.tmux.lua
 
 return {
-	font = wezterm.font_with_fallback({
-		-- { family = "JetBrains Mono", weight = "Medium" },
-		{ family = "MonoLisa" },
-		-- { family = "Fira Code", weight = "Regular" },
-	}),
+	-- font = wezterm.font_with_fallback({
+	-- 	-- { family = "JetBrains Mono", weight = "Medium" },
+	-- 	{ family = "Liga SFMono Nerd Font" },
+	-- 	-- { family = "Fira Code", weight = "Regular" },
+	-- }),
+	--
+	-- -- font_rules = {
+	-- -- 	intensity = "Bold",
+	-- -- 	italic = false,
+	-- -- 	font = wezterm.font_with_fallback({
+	-- -- 		family = "Liga SFMono Nerd Font",
+	-- -- 		weight = 900,
+	-- -- 	}),
+	-- -- },
+	--
+	-- font_rules = {
+	-- 	{
+	-- 		intensity = "Bold",
+	-- 		italic = false,
+	-- 		font = wezterm.font_with_fallback({
+	-- 			family = "Liga SFMono Nerd Font",
+	-- 			weight = "Bold",
+	-- 		}),
+	-- 	},
+	-- },
+
+	font = wezterm.font_with_fallback({ { family = "Liga SFMono Nerd Font" } }),
+	font_rules = {
+		-- Bold-not-italic
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font({
+				family = "Liga SFMono Nerd Font",
+				weight = "Bold",
+			}),
+		},
+
+		-- Bold-and-italic
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font({
+				family = "Liga SFMono Nerd Font",
+				italic = true,
+			}),
+		},
+
+		-- normal-intensity-and-italic
+		{
+			intensity = "Normal",
+			italic = true,
+			font = wezterm.font({
+				family = "Liga SFMono Nerd Font",
+				weight = "Regular",
+				italic = true,
+			}),
+		},
+
+		-- half-intensity-and-italic (half-bright or dim); use a lighter weight font
+		{
+			intensity = "Half",
+			italic = true,
+			font = wezterm.font({
+				family = "Liga SFMono Nerd Font",
+				weight = "Medium",
+				italic = true,
+			}),
+		},
+
+		-- half-intensity-and-not-italic
+		{
+			intensity = "Half",
+			italic = false,
+			font = wezterm.font({
+				family = "Liga SFMono Nerd Font",
+				weight = "Medium",
+			}),
+		},
+	},
 
 	font_size = 13.0,
-	cell_width = 0.90,
+	cell_width = 1,
 	line_height = 1.2,
 	color_scheme = "nord",
-	dpi = 144.0,
+	-- dpi = 144.0,
 
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
@@ -30,26 +105,26 @@ return {
 		-- Get new keys with `xxd -psd`
 		-- { key = "l", mods = "LEADER", action = wezterm.action.ShowLauncherArgs{ flags = "FUZZY|KEY_ASSIGNMENTS" } },
 		{ key = "l", mods = "LEADER", action = wezterm.action.ShowLauncher },
-		{ key = "1", mods = "CMD", action = wezterm.action.SendString("\x01\x31") }, -- <prefix>1
-		{ key = "2", mods = "CMD", action = wezterm.action.SendString("\x01\x32") }, -- <prefix>2
-		{ key = "3", mods = "CMD", action = wezterm.action.SendString("\x01\x33") }, -- <prefix>3
-		{ key = "4", mods = "CMD", action = wezterm.action.SendString("\x01\x34") }, -- <prefix>4
-		{ key = "9", mods = "CMD", action = wezterm.action.SendString("\x01\x39") }, -- <prefix>9
+		{ key = "1", mods = "CMD", action = wezterm.action.SendString("\x13\x31") }, -- <prefix>1
+		{ key = "2", mods = "CMD", action = wezterm.action.SendString("\x13\x32") }, -- <prefix>2
+		{ key = "4", mods = "CMD", action = wezterm.action.SendString("\x13\x33") }, -- <prefix>3
+		{ key = "4", mods = "CMD", action = wezterm.action.SendString("\x13\x34") }, -- <prefix>4
+		{ key = "9", mods = "CMD", action = wezterm.action.SendString("\x13\x39") }, -- <prefix>9
 
 		-- New tmux tab
-		{ key = "t", mods = "CMD", action = wezterm.action.SendString("\x01\x63") }, -- <prefix>t
+		{ key = "t", mods = "CMD", action = wezterm.action.SendString("\x13\x63") }, -- <prefix>t
 
 		-- Close tmux tab
-		{ key = "x", mods = "CMD", action = wezterm.action.SendString("\x01\x78") }, -- <prefix>x
+		{ key = "x", mods = "CMD", action = wezterm.action.SendString("\x13\x78") }, -- <prefix>x
 
 		-- Fuzzy find zoxide sessionizer
-		{ key = "f", mods = "CMD", action = wezterm.action.SendString("\x01\x66") }, -- <prefix>f
-		{ key = "g", mods = "CMD", action = wezterm.action.SendString("\x01\x67") }, -- <prefix>g
+		{ key = "f", mods = "CMD", action = wezterm.action.SendString("\x13\x66") }, -- <prefix>f
+		{ key = "g", mods = "CMD", action = wezterm.action.SendString("\x13\x67") }, -- <prefix>g
 
-		{ key = "m", mods = "CMD", action = wezterm.action.SendString("\x01\x6d") }, -- <prefix>g
+		{ key = "m", mods = "CMD", action = wezterm.action.SendString("\x13\x6d") }, -- <prefix>g
 
 		-- Tmux switch last session
-		{ key = "z", mods = "CMD", action = wezterm.action.SendString("\x01\x7a") }, -- <prefix>z
+		{ key = "z", mods = "CMD", action = wezterm.action.SendString("\x13\x7a") }, -- <prefix>z
 
 		-- cmd p
 		{ key = "p", mods = "CMD", action = wezterm.action.SendString("\x10") }, -- <prefix>p
